acefd9d9cd930b6d316a19231cf49ac0912ad319
Freddie <fthompson803@live.com>
2020-09-24 22:54:42 -0400

    adding social mvp

--- projects/ancestor/test_ancestor.py
+++ projects/ancestor/test_ancestor.py
@@ -19,13 +19,13 @@ class Test(unittest.TestCase):
                           (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
         self.assertEqual(earliest_ancestor(test_ancestors, 1), 10)
         self.assertEqual(earliest_ancestor(test_ancestors, 2), -1)
-        '''self.assertEqual(earliest_ancestor(test_ancestors, 3), 10)'''
+        self.assertEqual(earliest_ancestor(test_ancestors, 3), 10)
         self.assertEqual(earliest_ancestor(test_ancestors, 4), -1)
         self.assertEqual(earliest_ancestor(test_ancestors, 5), 4)
-        '''self.assertEqual(earliest_ancestor(test_ancestors, 6), 10)'''
+        self.assertEqual(earliest_ancestor(test_ancestors, 6), 10)
         self.assertEqual(earliest_ancestor(test_ancestors, 7), 4)
-        '''self.assertEqual(earliest_ancestor(test_ancestors, 8), 4)'''
-        '''self.assertEqual(earliest_ancestor(test_ancestors, 9), 4)'''
+        self.assertEqual(earliest_ancestor(test_ancestors, 8), 4)
+        self.assertEqual(earliest_ancestor(test_ancestors, 9), 4)
         self.assertEqual(earliest_ancestor(test_ancestors, 10), -1)
         self.assertEqual(earliest_ancestor(test_ancestors, 11), -1)
 
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,27 @@
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
 
 class SocialGraph:
     def __init__(self):
@@ -28,6 +49,13 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def fisher_yates(self, arr):
+        copy_arr = list(arr)
+        for idx in range(len(copy_arr)):
+            rand_index = random.randint(0, len(copy_arr)-1)
+            copy_arr[idx], copy_arr[rand_index] = copy_arr[rand_index], copy_arr[idx]
+        return copy_arr
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -45,8 +73,30 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(i)
 
         # Create friendships
+        all_friend_combos = []
+
+        for person in range(1, num_users):
+            for friend in range(person+1, num_users + 1):
+                all_friend_combos.append((person, friend))
+
+        # shuffle
+        shuffled_combos = self.fisher_yates(all_friend_combos)
+
+        # grab n number of elements
+        total_friendships = num_users * avg_friendships
+        elements_needed = total_friendships // 2
+        combos_to_make = shuffled_combos[:elements_needed]
+
+        # loop over and add friends
+
+        for friendship in combos_to_make:
+            self.add_friendship(friendship[0], friendship[1])
+
+        # total friendships
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +109,23 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        q = Queue()
+        q.enqueue([user_id])
+
+        while q.size() > 0:
+            current_path = q.dequeue()
+            current_user = current_path[-1]
+
+            if current_user not in visited:
+                visited[current_user] = current_path
+
+                friends = self.friendships[current_user]
+
+                for friend in friends:
+                    path_copy = list(current_path)
+                    path_copy.append(friend)
+                    q.enqueue(path_copy)
+        # want a breadth first traversal, because we want to visit every node in shortest time
         return visited
 
 
